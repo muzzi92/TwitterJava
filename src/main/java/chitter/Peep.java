a9bab5d2c07f178ff28009336f22bf58df1c80df
Muzzi Aldean <amiraldean@gmail.com>
2018-06-10 19:13:21 +0100

    change repo. created form that posts tweets to view

---
+++ src/main/java/chitter/Peep.java
@@ -0,0 +1,14 @@
+package chitter;
+
+public class Peep {
+
+    private String content;
+
+    public String getContent(){
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/chitter/PeepController.java
@@ -0,0 +1,20 @@
+package chitter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class PeepController {
+    @GetMapping("/post")
+    public String peepForm(Model model) {
+        model.addAttribute("peep", new Peep());
+        return "post";
+    }
+    @PostMapping("/post")
+    public String peepSubmit(@ModelAttribute Peep peep) {
+        return "peep";
+    }
+}
